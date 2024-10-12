08f0c7071fe7b42e518f3e795f37e861afbad1fb
talineshawwaa <talineshawwa15@gmail.com>
2024-10-12 20:38:54 +0200

    Edits to package.json and vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,8 @@ module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
     devtool: "source-map"
-  }
+  },
+  outputDir: process.env.NODE_ENV === 'development' ? 'dist-dev' :
+             process.env.NODE_ENV === 'uat' ? 'dist-uat' :
+             'dist', // fallback to dist for production
 })

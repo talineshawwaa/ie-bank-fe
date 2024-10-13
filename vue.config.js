79cae796d9cfc77080fe8bab1ebda5ec048adb3e
talineshawwaa <talineshawwa15@gmail.com>
2024-10-13 18:37:21 +0200

    changes for uat+dev deployment

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
-
 module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
@@ -7,5 +6,5 @@ module.exports = defineConfig({
   },
   outputDir: process.env.NODE_ENV === 'development' ? 'dist-dev' :
              process.env.NODE_ENV === 'uat' ? 'dist-uat' :
-             'dist', // fallback to 'dist' for production
+             'dist', // fallback to dist for production
 })

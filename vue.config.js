071e34f12ac79115607ce97d7fa6a36fe06415cf
talineshawwaa <talineshawwa15@gmail.com>
2024-10-14 16:44:46 +0200

    changes for uat+dev deployment

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   configureWebpack: {
     devtool: "source-map"
   },
-  outputDir: process.env.NODE_ENV === 'development' ? 'dist-dev' :
-             process.env.NODE_ENV === 'uat' ? 'dist-uat' :
+  outputDir: process.env.VUE_APP_ENV_VAR_FILE_NAME === '.env.uat' ? 'dist-uat' :
+             process.env.NODE_ENV === 'development' ? 'dist-dev' :
              'dist', // fallback to dist for production
 })
